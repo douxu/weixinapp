4a204eab4dc0896d1fdb20a4746c247eb0573827
douxu <douxu@users.noreply.github.com>
2017-04-06 22:04:30 +0800

    Update router.go
    update

--- routers/router.go
+++ routers/router.go
@@ -8,5 +8,5 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/api/login/VerifyLoginInfo", &controllers.UserController{})
+	beego.Router("/api/login/VerifyLoginInfo", &controllers.UserController{},"post:LoginVerify")
 }
